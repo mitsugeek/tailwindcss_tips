3e3c232c176340992517bd6285a798dd50f8494a
mitsugeek <mitsu322@gmail.com>
2021-11-08 18:19:55 +0900

    不要ファイル削除、ヘッダ固定サンプル作成

--- tailwindcss_tips/src/App.vue
+++ tailwindcss_tips/src/App.vue
@@ -1,21 +1,98 @@
+<template>
+  <div class="h-screen overflow-hidden bg-white">
+    <h1>Tailwind CSS TIPS</h1>
+    <div class="h-screen overflow-y-auto">
+      <div class="bg-gray-50  h-screen w-full"></div>
+      <div class="bg-gray-100 h-screen w-full"></div>
+      <div class="bg-gray-200 h-screen w-full"></div>
+      <div class="bg-gray-300 h-screen w-full"></div>
+      <div class="bg-gray-400 h-screen w-full"></div>
+      <div class="bg-gray-500 h-screen w-full"></div>
+      <div class="bg-gray-600 h-screen w-full"></div>
+      <div class="bg-gray-700 h-screen w-full"></div>
+      <div class="bg-gray-800 h-screen w-full"></div>
+      <div class="bg-gray-900 h-screen w-full"></div>
+      
+      <div class="bg-red-50  h-screen w-full"></div>
+      <div class="bg-red-100 h-screen w-full"></div>
+      <div class="bg-red-200 h-screen w-full"></div>
+      <div class="bg-red-300 h-screen w-full"></div>
+      <div class="bg-red-400 h-screen w-full"></div>
+      <div class="bg-red-500 h-screen w-full"></div>
+      <div class="bg-red-600 h-screen w-full"></div>
+      <div class="bg-red-700 h-screen w-full"></div>
+      <div class="bg-red-800 h-screen w-full"></div>
+      <div class="bg-red-900 h-screen w-full"></div>
+      
+      <div class="bg-yellow-50  h-screen w-full"></div>
+      <div class="bg-yellow-100 h-screen w-full"></div>
+      <div class="bg-yellow-200 h-screen w-full"></div>
+      <div class="bg-yellow-300 h-screen w-full"></div>
+      <div class="bg-yellow-400 h-screen w-full"></div>
+      <div class="bg-yellow-500 h-screen w-full"></div>
+      <div class="bg-yellow-600 h-screen w-full"></div>
+      <div class="bg-yellow-700 h-screen w-full"></div>
+      <div class="bg-yellow-800 h-screen w-full"></div>
+      <div class="bg-yellow-900 h-screen w-full"></div>
+      
+      <div class="bg-green-50  h-screen w-full"></div>
+      <div class="bg-green-100 h-screen w-full"></div>
+      <div class="bg-green-200 h-screen w-full"></div>
+      <div class="bg-green-300 h-screen w-full"></div>
+      <div class="bg-green-400 h-screen w-full"></div>
+      <div class="bg-green-500 h-screen w-full"></div>
+      <div class="bg-green-600 h-screen w-full"></div>
+      <div class="bg-green-700 h-screen w-full"></div>
+      <div class="bg-green-800 h-screen w-full"></div>
+      <div class="bg-green-900 h-screen w-full"></div>
+      
+      <div class="bg-blue-50  h-screen w-full"></div>
+      <div class="bg-blue-100 h-screen w-full"></div>
+      <div class="bg-blue-200 h-screen w-full"></div>
+      <div class="bg-blue-300 h-screen w-full"></div>
+      <div class="bg-blue-400 h-screen w-full"></div>
+      <div class="bg-blue-500 h-screen w-full"></div>
+      <div class="bg-blue-600 h-screen w-full"></div>
+      <div class="bg-blue-700 h-screen w-full"></div>
+      <div class="bg-blue-800 h-screen w-full"></div>
+      <div class="bg-blue-900 h-screen w-full"></div>
+      
+      <div class="bg-indigo-50  h-screen w-full"></div>
+      <div class="bg-indigo-100 h-screen w-full"></div>
+      <div class="bg-indigo-200 h-screen w-full"></div>
+      <div class="bg-indigo-300 h-screen w-full"></div>
+      <div class="bg-indigo-400 h-screen w-full"></div>
+      <div class="bg-indigo-500 h-screen w-full"></div>
+      <div class="bg-indigo-600 h-screen w-full"></div>
+      <div class="bg-indigo-700 h-screen w-full"></div>
+      <div class="bg-indigo-800 h-screen w-full"></div>
+      <div class="bg-indigo-900 h-screen w-full"></div>
+      
+      <div class="bg-purple-50  h-screen w-full"></div>
+      <div class="bg-purple-100 h-screen w-full"></div>
+      <div class="bg-purple-200 h-screen w-full"></div>
+      <div class="bg-purple-300 h-screen w-full"></div>
+      <div class="bg-purple-400 h-screen w-full"></div>
+      <div class="bg-purple-500 h-screen w-full"></div>
+      <div class="bg-purple-600 h-screen w-full"></div>
+      <div class="bg-purple-700 h-screen w-full"></div>
+      <div class="bg-purple-800 h-screen w-full"></div>
+      <div class="bg-purple-900 h-screen w-full"></div>
+      
+      <div class="bg-pink-50  h-screen w-full"></div>
+      <div class="bg-pink-100 h-screen w-full"></div>
+      <div class="bg-pink-200 h-screen w-full"></div>
+      <div class="bg-pink-300 h-screen w-full"></div>
+      <div class="bg-pink-400 h-screen w-full"></div>
+      <div class="bg-pink-500 h-screen w-full"></div>
+      <div class="bg-pink-600 h-screen w-full"></div>
+      <div class="bg-pink-700 h-screen w-full"></div>
+      <div class="bg-pink-800 h-screen w-full"></div>
+      <div class="bg-pink-900 h-screen w-full"></div>
+    </div>
+  </div>
+</template>
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
